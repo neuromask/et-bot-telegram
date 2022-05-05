e96adb2d6dafadec3c2dc9e0d1a3c3e676d959f7
Aleksandr <neuromask@gmail.com>
2022-05-05 13:22:41 +0300

    weather

--- components/weather.js
+++ components/weather.js
@@ -24,8 +24,9 @@ module.exports = {
                 let feels = Math.round(data.weather.feels_like.cur)
                 let answer = `ET⚡️ *Weather*\n\n`
                 answer += `• ${translate('weather.descriptionText', ctx)} *${data.weather.description}*\n`
-                answer += `• ${translate('weather.tempText', ctx)} *${temp}\u00B0С*\n`
-                answer += `• ${translate('weather.feelsText', ctx)} *${feels}\u00B0С*\n\n`
+                answer += `• 🌡️ ${translate('weather.tempText', ctx)} *${temp}\u00B0С*\n`
+                answer += `• 🛴 ${translate('weather.feelsText', ctx)} *${feels}\u00B0С*\n`
+                answer += `• 🌎 ${translate('weather.details', ctx)}: *https://bit.ly/3scCWBW*\n\n`
                 if (temp < -15) answer += `${translate('weather.m15', ctx)}`
                 else if (temp < -10) answer += `${translate('weather.m10', ctx)}`
                 else if (temp < -5) answer += `${translate('weather.m5', ctx)}`
@@ -36,7 +37,8 @@ module.exports = {
                 else if (temp > 15) answer += `${translate('weather.15', ctx)}`
                 else if (temp > 20) answer += `${translate('weather.20', ctx)}`
                 else if (temp > 25) answer += `${translate('weather.25', ctx)}`
-                bot.telegram.sendMessage(ctx.chat.id, answer, { parse_mode: "Markdown" });
+                
+                bot.telegram.sendMessage(ctx.chat.id, answer, { parse_mode: "Markdown", disable_web_page_preview:"True" });
                 //ctx.replyWithPhoto({ url: `http://openweathermap.org/img/wn/${data.weather.icon.raw}@4x.png`}, { caption: data.weather.description });
                 
             })
